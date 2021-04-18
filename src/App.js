d4ad92eb38ea1b66a796323a64a18ab62b924e07
chrockny <chrockny1007@gmail.com>
2021-04-18 01:53:04 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita  <code>src/App.js</code> y guarda para el reload.
+
+          xdddd
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
