6bd02cee9f64aec7651284fd7f6c9514ef4f9c06
chrockny <chrockny1007@gmail.com>
2021-04-18 02:03:37 -0500

    Hicimos nuevos cambios en el repositorio de Prework.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita  <code>src/App.js</code> y guarda para el reload.
-
-          xdddd
+          Edita  <code>src/App.js</code> y guarda para el reload
+          .
         </p>
+<p>
+  dmadpawmdwmdawmdopmawd
+  ad
+  awmd
+  oawmdo
+  wmd
+  oawmd
+  omawdoawdomawd
+  mawdomaw´
+  domaw
+  ´dmwadmawopdmawmdoawdmoawdm
+  aw
+        </p>
+        <h2>Bienvenido  a un nuevo cambio de Rockny el programador</h2>
       </header>
     </div>
+
   );
 }
 
